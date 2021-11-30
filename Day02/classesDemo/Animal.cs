713f9e2e6f2979fae235c6bc62719301cbdde768
NicholeKing <nichole.king94@hotmail.com>
2021-11-30 14:06:19 -0500

    Day Two Classes

---
+++ Day02/classesDemo/Animal.cs
@@ -0,0 +1,70 @@
+using System;
+
+namespace classesDemo
+{
+    public class Animal
+    {
+        private string species;
+        public int numLegs;
+        private double weight;
+        public double Weight
+        {
+            get {return weight;}
+        }
+        public bool isMammal;
+        public string color;
+        public int numEyes = 0;
+
+        public Animal(string sp, int legs, double wgt, bool isMam, string clr)
+        {
+            species = sp;
+            numLegs = legs;
+            weight = wgt;
+            isMammal = isMam;
+            color = clr;
+        }
+
+        public Animal(string sp, double wgt, string clr)
+        {
+            species = sp;
+            numLegs = 4;
+            weight = wgt;
+            isMammal = true;
+            color = clr;
+        }
+
+        public Animal(string sp, double wgt, bool isMam, string clr)
+        {
+            species = sp;
+            numLegs = 2;
+            weight = wgt;
+            isMammal = isMam;
+            color = clr;
+        }
+
+        // public Animal(string clr = "none", int numLegs = 0)
+        // {
+        //     species = "fjdis";
+        //     numLegs = numLegs;
+        //     weight = 12.23;
+        //     isMammal = true;
+        //     color = clr;
+        // }
+
+        // Methods that the animal can do
+        public void ShowStats()
+        {
+            Console.WriteLine($"Species:   {species}");
+            Console.WriteLine($"# of Legs: {numLegs}");
+            Console.WriteLine($"Weight:    {weight} lbs");
+            Console.WriteLine($"Mammal:    {isMammal}");
+            Console.WriteLine($"Color:     {color}");
+            Console.WriteLine("*******************");
+        }
+
+        public void feed()
+        {
+            weight += 1.2;
+        }
+    }
+}
---
+++ Day02/classesDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace classesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal a = new Animal("Lion", 250.5, "yellow");
+            // Console.WriteLine(a.species);
+            a.ShowStats();
+            Animal b = new Animal("House cat", 4, 8.4, true, "white");
+            // b.species = "Red panda";
+            b.ShowStats();
+            Console.WriteLine(b.Weight);
+            b.feed();
+            Console.WriteLine(b.Weight);
+            // Imagine you're playing a video game
+            // Your character has a health amount and your enemy has a health amount
+            // When you get hit, you take damage - your health goes down
+            // You have a health variable that you are able to modify
+            // What if your enemy keeps resetting their health to 100? 
+            // What if your enemy makes it so their hits take your health down to 0?
+        }
+    }
+}
